665682abc7856cba0cba9b92a1103fcaf7eead74
vivek-kanjariya <kanjariyavivek295@gmail.com>
2024-02-05 13:48:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setdata] = useState([]);
+  const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw&type=single";
+
+  const yo = () => {
+    axios.get(url).then((result) => {
+      setdata(result.data);
+      console.log(result);
+    });
+  };
+  function red(){
+    window.location.href = "https://youtu.be/M8uiTN40Sg0?si=5mQ9VljH5OYreYrS";
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <img
+          src={
+            "https://i.pinimg.com/474x/90/33/83/903383dbe84483ffe3628a9149a55a1e.jpg"
+          }
+          alt="check your internet or throw your phone"
+          className="top"
+        />
+
+        <div className="bottom">
+          <div className="main">{data.joke}</div>
+          <div className="info">
+            <button className="btn1" onClick={red}>Dankonce</button>
+            <button className="btn2" onClick={yo}>
+              next
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
